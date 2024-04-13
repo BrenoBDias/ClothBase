05122049df5f1b016f11fe6b9f53226ce62c27ed
BrenoBDias <99230311+BrenoBDias@users.noreply.github.com>
2024-04-13 10:57:51 -0300

    clothbasedb working and ready to beeing developed

--- clothbase.sql
+++ clothbase.sql
@@ -1,12 +1,30 @@
-CREATE TABLE accounts(
-    user_id serial PRIMARY KEY,
-    username VARCHAR( 50 ) UNIQUE NOT NULL,
-    password VARCHAR( 50 ) NOT NULL,
-    email VARCHAR( 255 ) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL,
-        last_login TIMESTAMP
+CREATE TABLE folder(
+    folder_id SERIAL PRIMARY KEY,
+    FOREIGN KEY(root_folder) REFERENCES folder(folder_id),
+    folder_name VARCHAR( 50 ) UNIQUE NOT NULL,
+    root_folder INT
 
 );
 
-INSERT INTO accounts(username, password, email, created_on)
-VALUES('user', 'pass', 'test@mail.com', CURRENT_TIMESTAMP);
+INSERT INTO folder(folder_name,root_folder)  
+VALUES  ('root', NULL),
+        ('folder one', 1);
+
+
+CREATE TABLE product(
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) UNIQUE NOT NULL,
+    product_description VARCHAR(500),
+    created_on TIMESTAMP NOT NULL
+);
+
+CREATE TABLE custumer(
+    custumer_id SERIAL PRIMARY KEY,
+    custumer_name VARCHAR(50) UNIQUE NOT NULL,
+    custumer_adress VARCHAR(100),
+    custumer_contact_info VARCHAR(50) UNIQUE,
+    custumer_email VARCHAR(50) UNIQUE,
+    custumer_cpf VARCHAR(50) UNIQUE NOT NULL,
+    registred_on TIMESTAMP NOT NULL,
+        last_modified TIMESTAMP    
+);
