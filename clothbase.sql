eeef6c937869304904d825428af315c991d8073e
BrenoBDias <99230311+BrenoBDias@users.noreply.github.com>
2024-04-13 11:53:00 -0300

    finished database skeleton, ready to populate it

--- clothbase.sql
+++ clothbase.sql
@@ -1,8 +1,8 @@
 CREATE TABLE folder(
     folder_id SERIAL PRIMARY KEY,
+    root_folder INT,
     FOREIGN KEY(root_folder) REFERENCES folder(folder_id),
-    folder_name VARCHAR( 50 ) UNIQUE NOT NULL,
-    root_folder INT
+    folder_name VARCHAR( 50 ) UNIQUE NOT NULL
 
 );
 
@@ -18,6 +18,7 @@ CREATE TABLE product(
     created_on TIMESTAMP NOT NULL
 );
 
+
 CREATE TABLE custumer(
     custumer_id SERIAL PRIMARY KEY,
     custumer_name VARCHAR(50) UNIQUE NOT NULL,
@@ -27,4 +28,65 @@ CREATE TABLE custumer(
     custumer_cpf VARCHAR(50) UNIQUE NOT NULL,
     registred_on TIMESTAMP NOT NULL,
         last_modified TIMESTAMP    
+);
+
+CREATE TABLE project(
+    project_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    custumer_id INT,
+    FOREIGN KEY(custumer_id) REFERENCES custumer(custumer_id),
+    project_name VARCHAR(50) UNIQUE,
+    project_description VARCHAR(500),
+    registred_on TIMESTAMP NOT NULL,
+        last_modified TIMESTAMP
+);
+
+CREATE TABLE stock_out(
+    stock_out_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    project_id INT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES project(project_id),
+    stock_out_quantity INT NOT NULL,
+    registred_on TIMESTAMP NOT NULL
+);
+
+CREATE TABLE image(
+    image_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    project_id INT,
+    FOREIGN KEY (project_id) REFERENCES project(project_id),
+    image_data INT NOT NULL,
+    registred_on TIMESTAMP NOT NULL
+);
+
+CREATE TABLE tag(
+    tag_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    project_id INT,
+    FOREIGN KEY (project_id) REFERENCES project(project_id),
+    tag_name VARCHAR (15)
+);
+
+CREATE TABLE supplier(
+    supplier_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    supplier_name VARCHAR(50) UNIQUE NOT NULL,
+    supplier_contact_info VARCHAR (50),
+    supplier_adress VARCHAR(100) UNIQUE,
+    supplier_email VARCHAR(50) UNIQUE,
+    supplier_site VARCHAR(50) UNIQUE,
+    registred_on TIMESTAMP NOT NULL,
+        last_modified TIMESTAMP
+);
+
+CREATE TABLE stock_in(
+    stock_in_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    project_id INT,
+    FOREIGN KEY (project_id) REFERENCES project(project_id),
+    quantity INT NOT NULL,
+    stock_in_unit_price NUMERIC(5,2)
+
 );
